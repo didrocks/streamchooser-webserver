2f22cffae4bf550ca6b3b39da3c5f6194d0e2fbc
Didier Roche <didrocks@ubuntu.com>
2016-03-21 17:44:42 +0100

    Support cwd != exec dir

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"os"
+	"path"
+	"path/filepath"
 	"strings"
 )
 
+var serviceDir string
+var pagePath string
 var currentStream string
 
 const port = ":8041"
@@ -19,13 +25,24 @@ type streamInfo struct {
 func handler(w http.ResponseWriter, r *http.Request) {
 	currentStream = r.FormValue("url")
 
-	t, _ := template.ParseFiles("mainpage.tmpl")
+	t, _ := template.ParseFiles(pagePath)
 	formInfo := streamInfo{Current: currentStream, Host: strings.TrimSuffix(r.Host, port), Playing: currentStream != ""}
 	t.Execute(w, formInfo)
 }
 
 func main() {
+	serviceDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	pagePath = path.Join(serviceDir, "mainpage.tmpl")
+	// assuming it's running from cwd if go run is used
+	if _, err := os.Stat(pagePath); os.IsNotExist(err) {
+		serviceDir = "."
+		pagePath = path.Join(serviceDir, "mainpage.tmpl")
+	}
+
 	http.HandleFunc("/", handler)
-	http.Handle("/static/", http.FileServer(http.Dir("./static")))
+	http.Handle("/static/", http.FileServer(http.Dir(path.Join(serviceDir, "static"))))
 	http.ListenAndServe(port, nil)
 }
