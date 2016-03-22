41c3df63b7e8c220af5fe2942d59b828a5701a58
Didier Roche <didrocks@ubuntu.com>
2016-03-22 10:47:15 +0100

    Handle refreshing stream file and kill existing vlc process group

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"path"
 	"path/filepath"
 	"strings"
@@ -17,14 +18,38 @@ var currentStream string
 
 const port = ":8041"
 
+// those 2 files are in pwd
+const streamFileName = "input_video"
+const pidFile = "vlc.pid"
+
 type streamInfo struct {
 	Current string
 	Host    string
 	Playing bool
 }
 
+func refreshStream() {
+	// Write new stream file
+	ioutil.WriteFile(streamFileName, []byte("STREAM_URL="+currentStream), 0644)
+
+	// Kill whole process group (hence no use of signal API) referenced in vlc.pid
+	content, err := ioutil.ReadFile(pidFile)
+	if err == nil {
+		gpid := strings.TrimSuffix(string(content), "\n")
+		cmd := exec.Command("kill", "-9", "--", "-"+gpid)
+		err := cmd.Run()
+		if err != nil {
+			log.Println("Couldn't kill "+gpid+". Error:", err)
+		}
+	}
+}
+
+/* Serve current handler form and refresh the stream file */
 func handler(w http.ResponseWriter, r *http.Request) {
-	currentStream = r.FormValue("url")
+	if r.Method == "POST" {
+		currentStream = r.FormValue("url")
+		go refreshStream()
+	}
 
 	formInfo := streamInfo{Current: currentStream, Host: strings.TrimSuffix(r.Host, port), Playing: currentStream != ""}
 	templateFile.Execute(w, formInfo)
